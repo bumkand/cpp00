0f1939160fc1cea7c526ec6d0e5bfb369cf9bc5d
bumkand <jakub.andrasi@gmail.com>
2026-01-08 20:52:18 +0100

    ex00 updated

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,22 +3,19 @@
 
 int	main(int arc, char *arg[])
 {
-	int	i = 1;
-	int	j = 0;
-
 	if (arc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (i = 1; i < arc; i++)
+	for (int i = 1; i < arc; i++)
 	{
+		int j = 0;
 		while (arg[i][j])
 		{
 			std::cout << char(toupper(arg[i][j]));
 			j++;
 		}
-		j = 0;
 	}
 	std::cout << std::endl;
 	return (0);
